27177bf9c8c487ef6bca9c38dbd78a40746662af
Pavel Gnusin <pavel.sin.gnusin@gmail.com>
2022-04-22 00:32:39 +0300

    lesson1 - реализация Vec2D

--- test_scene.cpp
+++ test_scene.cpp
@@ -9,7 +9,7 @@ int main() {
 
     sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
     sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    shape.setFillColor(sf::Color::Blue);
 
     while (window.isOpen())
     {
