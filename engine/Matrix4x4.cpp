938daeba8b28e5371d875b80108004b46a5e9f68
Gnusin Pavel <pavel.sin.gnusin@gmail.com>
2022-04-22 13:15:01 +0300

    lesson1 - Matrix4x4

--- engine/Matrix4x4.cpp
+++ engine/Matrix4x4.cpp
@@ -8,28 +8,42 @@
 #include "Matrix4x4.h"
 #include "Consts.h"
 
-Matrix4x4 Matrix4x4::operator*(const Matrix4x4 &matrix4X4) const {
+Matrix4x4 Matrix4x4::operator*(const Matrix4x4& matrix4X4) const {
     Matrix4x4 result = Matrix4x4::Zero();
-
-    // TODO: implement  (lesson 1)
-
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            for (int k = 0; k < 4; ++k) {
+                result._arr[i][j] += _arr[i][k] * matrix4X4._arr[k][j];
+            }
+        }
+    }
     return result;
 }
 
-Vec4D Matrix4x4::operator*(const Vec4D &point4D) const {
-    // TODO: implement (lesson 1)
-    return Vec4D();
+Vec4D Matrix4x4::operator*(const Vec4D& point4D) const {
+    return Vec4D(
+        _arr[0][0] * point4D.x() + _arr[0][1] * point4D.y() + _arr[0][2] * point4D.z() + _arr[0][3] * point4D.w(),
+        _arr[1][0] * point4D.x() + _arr[1][1] * point4D.y() + _arr[1][2] * point4D.z() + _arr[1][3] * point4D.w(),
+        _arr[2][0] * point4D.x() + _arr[2][1] * point4D.y() + _arr[2][2] * point4D.z() + _arr[2][3] * point4D.w(),
+        _arr[3][0] * point4D.x() + _arr[3][1] * point4D.y() + _arr[3][2] * point4D.z() + _arr[3][3] * point4D.w()
+    );
 }
 
-Vec3D Matrix4x4::operator*(const Vec3D &vec) const {
-    // TODO: implement (lesson 1)
-    return Vec3D();
+Vec3D Matrix4x4::operator*(const Vec3D& vec) const {
+    return Vec3D(
+        _arr[0][0] * vec.x() + _arr[0][1] * vec.y() + _arr[0][2] * vec.z(),
+        _arr[1][0] * vec.x() + _arr[1][1] * vec.y() + _arr[1][2] * vec.z(),
+        _arr[2][0] * vec.x() + _arr[2][1] * vec.y() + _arr[2][2] * vec.z()
+    );
 }
 
 Matrix4x4 Matrix4x4::Identity() {
-    Matrix4x4 result;
+    Matrix4x4 result = Matrix4x4::Zero();
 
-    // TODO: implement (lesson 1)
+    result._arr[0][0] = 1;
+    result._arr[1][1] = 1;
+    result._arr[2][2] = 1;
+    result._arr[3][3] = 1;
 
     return result;
 }
@@ -37,7 +51,11 @@ Matrix4x4 Matrix4x4::Identity() {
 Matrix4x4 Matrix4x4::Constant(double value) {
     Matrix4x4 result;
 
-    // TODO: implement (lesson 1)
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            result._arr[i][j] = value;
+        }
+    }
 
     return result;
 }
@@ -46,18 +64,28 @@ Matrix4x4 Matrix4x4::Zero() {
     return Matrix4x4::Constant(0);
 }
 
-Matrix4x4 Matrix4x4::Scale(const Vec3D &factor) {
+Matrix4x4 Matrix4x4::Scale(const Vec3D& factor) {
     Matrix4x4 s{};
 
-    // TODO: implement (lesson 1)
+    s._arr[0][0] = factor.x();
+    s._arr[1][1] = factor.y();
+    s._arr[2][2] = factor.z();
+    s._arr[3][3] = 1;
 
     return s;
 }
 
-Matrix4x4 Matrix4x4::Translation(const Vec3D &v) {
+Matrix4x4 Matrix4x4::Translation(const Vec3D& v) {
     Matrix4x4 t{};
 
-    // TODO: implement (lesson 1)
+    t._arr[0][0] = 1;
+    t._arr[1][1] = 1;
+    t._arr[2][2] = 1;
+    t._arr[3][3] = 1;
+
+    t._arr[0][3] = v.x();
+    t._arr[1][3] = v.y();
+    t._arr[2][3] = v.z();
 
     return t;
 }
@@ -65,7 +93,17 @@ Matrix4x4 Matrix4x4::Translation(const Vec3D &v) {
 Matrix4x4 Matrix4x4::RotationX(double rx) {
     Matrix4x4 Rx{};
 
-    // TODO: implement (lesson 1)
+    double c = cos(rx);
+    double s = sin(rx);
+
+    Rx._arr[0][0] = 1;
+
+    Rx._arr[1][1] = c;
+    Rx._arr[1][2] = -s;
+    Rx._arr[2][1] = s;
+    Rx._arr[2][2] = c;
+
+    Rx._arr[3][3] = 1;
 
     return Rx;
 }
@@ -73,7 +111,17 @@ Matrix4x4 Matrix4x4::RotationX(double rx) {
 Matrix4x4 Matrix4x4::RotationY(double ry) {
     Matrix4x4 Ry{};
 
-    // TODO: implement (lesson 1)
+    double c = cos(ry);
+    double s = sin(ry);
+
+    Ry._arr[1][1] = 1;
+
+    Ry._arr[0][0] = c;
+    Ry._arr[0][2] = s;
+    Ry._arr[2][0] = -s;
+    Ry._arr[2][2] = c;
+
+    Ry._arr[3][3] = 1;
 
     return Ry;
 }
@@ -81,16 +129,26 @@ Matrix4x4 Matrix4x4::RotationY(double ry) {
 Matrix4x4 Matrix4x4::RotationZ(double rz) {
     Matrix4x4 Rz{};
 
-    // TODO: implement (lesson 1)
+    double c = cos(rz);
+    double s = sin(rz);
+
+    Rz._arr[2][2] = 1;
+
+    Rz._arr[0][0] = c;
+    Rz._arr[0][1] = -s;
+    Rz._arr[1][0] = s;
+    Rz._arr[1][1] = c;
+
+    Rz._arr[3][3] = 1;
 
     return Rz;
 }
 
-Matrix4x4 Matrix4x4::Rotation(const Vec3D &r) {
+Matrix4x4 Matrix4x4::Rotation(const Vec3D& r) {
     return RotationX(r.x()) * RotationY(r.y()) * RotationZ(r.z());
 }
 
-Matrix4x4 Matrix4x4::Rotation(const Vec3D &v, double rv) {
+Matrix4x4 Matrix4x4::Rotation(const Vec3D& v, double rv) {
     Matrix4x4 Rv{};
     Vec3D nv(v.normalized());
 
@@ -129,7 +187,7 @@ Matrix4x4 Matrix4x4::ScreenSpace(int width, int height) {
     return s;
 }
 
-Matrix4x4 Matrix4x4::View(const Vec3D &left, const Vec3D &up, const Vec3D &lookAt, const Vec3D &eye) {
+Matrix4x4 Matrix4x4::View(const Vec3D& left, const Vec3D& up, const Vec3D& lookAt, const Vec3D& eye) {
     Matrix4x4 V = Zero();
 
     // TODO: implement (lesson 4)
@@ -161,34 +219,34 @@ void Matrix4x4::test() {
     // testing matrix-vector multiplication:
     Vec4D v(4, 2, 3, 1);
 
-    Vec4D one = Matrix4x4::Identity()*v;
+    Vec4D one = Matrix4x4::Identity() * v;
     assert(isNear(one.x(), 4) && isNear(one.y(), 2) && isNear(one.z(), 3) && isNear(one.w(), 1));
 
-    Vec4D scale = Matrix4x4::Scale(Vec3D(1, 2, 3))*v;
+    Vec4D scale = Matrix4x4::Scale(Vec3D(1, 2, 3)) * v;
     assert(isNear(scale.x(), 4) && isNear(scale.y(), 4) && isNear(scale.z(), 9) && isNear(scale.w(), 1));
 
-    Vec4D zero = Matrix4x4::Zero()*v;
+    Vec4D zero = Matrix4x4::Zero() * v;
     assert(isNear(zero.x(), 0) && isNear(zero.y(), 0) && isNear(zero.z(), 0) && isNear(zero.w(), 0));
 
-    Vec4D translated = Matrix4x4::Translation(Vec3D(5, 4, 3))*v;
+    Vec4D translated = Matrix4x4::Translation(Vec3D(5, 4, 3)) * v;
     assert(isNear(translated.x(), 9) && isNear(translated.y(), 6) && isNear(translated.z(), 6) && isNear(translated.w(), 1));
 
     // testing matrix-matrix multiplication:
-    Matrix4x4 c1 = Matrix4x4::Constant(5)*Matrix4x4::Identity();
-    for(int i = 0; i < 4; i++)
-        for(int j = 0; j < 4; j++)
+    Matrix4x4 c1 = Matrix4x4::Constant(5) * Matrix4x4::Identity();
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
             assert(isNear(c1._arr[i][j], 5));
 
-    Matrix4x4 c2 = Matrix4x4::Zero()*Matrix4x4::Constant(1);
-    for(int i = 0; i < 4; i++)
-        for(int j = 0; j < 4; j++)
+    Matrix4x4 c2 = Matrix4x4::Zero() * Matrix4x4::Constant(1);
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
             assert(isNear(c2._arr[i][j], 0));
 
-    Matrix4x4 c3 = Matrix4x4::Scale(Vec3D(3, 3, 3))*Matrix4x4::Identity();
-    for(int i = 0; i < 4; i++)
-        for(int j = 0; j < 4; j++)
-            if(i==j)
-                if(i<3)
+    Matrix4x4 c3 = Matrix4x4::Scale(Vec3D(3, 3, 3)) * Matrix4x4::Identity();
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            if (i == j)
+                if (i < 3)
                     assert(isNear(c3._arr[i][j], 3));
                 else
                     assert(isNear(c3._arr[i][j], 1));
@@ -196,32 +254,32 @@ void Matrix4x4::test() {
                 assert(isNear(c3._arr[i][j], 0));
 
     // testing rotation matrices:
-    Vec4D i(1, 0,0);
-    Vec4D j(0, 1,0);
-    Vec4D k(0, 0,1);
+    Vec4D i(1, 0, 0);
+    Vec4D j(0, 1, 0);
+    Vec4D k(0, 0, 1);
 
     // rotation X:
-    Vec4D i_rx = Matrix4x4::RotationX(M_PI/2)*i;
-    Vec4D j_rx = Matrix4x4::RotationX(M_PI/2)*j;
-    Vec4D k_rx = Matrix4x4::RotationX(M_PI/2)*k;
+    Vec4D i_rx = Matrix4x4::RotationX(M_PI / 2) * i;
+    Vec4D j_rx = Matrix4x4::RotationX(M_PI / 2) * j;
+    Vec4D k_rx = Matrix4x4::RotationX(M_PI / 2) * k;
 
     assert(i_rx == i);
     assert(j_rx == k);
     assert(k_rx == -j);
 
     // rotation Y:
-    Vec4D i_ry = Matrix4x4::RotationY(M_PI/2)*i;
-    Vec4D j_ry = Matrix4x4::RotationY(M_PI/2)*j;
-    Vec4D k_ry = Matrix4x4::RotationY(M_PI/2)*k;
+    Vec4D i_ry = Matrix4x4::RotationY(M_PI / 2) * i;
+    Vec4D j_ry = Matrix4x4::RotationY(M_PI / 2) * j;
+    Vec4D k_ry = Matrix4x4::RotationY(M_PI / 2) * k;
 
     assert(i_ry == -k);
     assert(j_ry == j);
     assert(k_ry == i);
 
     // rotation Z:
-    Vec4D i_rz = Matrix4x4::RotationZ(M_PI/2)*i;
-    Vec4D j_rz = Matrix4x4::RotationZ(M_PI/2)*j;
-    Vec4D k_rz = Matrix4x4::RotationZ(M_PI/2)*k;
+    Vec4D i_rz = Matrix4x4::RotationZ(M_PI / 2) * i;
+    Vec4D j_rz = Matrix4x4::RotationZ(M_PI / 2) * j;
+    Vec4D k_rz = Matrix4x4::RotationZ(M_PI / 2) * k;
 
     assert(i_rz == j);
     assert(j_rz == -i);
